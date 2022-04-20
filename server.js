64d212c8a2a2cc5083ce3252db2c3cff0d2a0b2a
klmitchy <karenapolis@gmail.com>
2022-04-20 14:55:00 -0500

    fix

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Public'));
 app.listen(PORT, () => console.log(`Listening on PORT: 3000`));
