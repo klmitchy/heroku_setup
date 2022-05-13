7863b075d7c5cb4592efc3e397affb5f9d76372a
klmitchy <karenapolis@gmail.com>
2022-05-12 21:07:43 -0500

    re work api route with tutor help

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,35 +2,28 @@ const router = require('express').Router();
 //const app = express();
 const fs = require("fs");
 //const path = require('path');
-
+const notes = [];
 
 router.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", function(err, data){
         //console.log(data)
-        const notes = [].concat(JSON.parse(data))
+        notes.push(JSON.parse(data))
         res.json(notes);
     })
-    /*.then(function(data){
-        console.log(data)
-        notes = [].concat(json.parse(data))
-        res.json(notes);
-    })*/
+   
 });
 
 
-router.post("/notes", (req, res) =>{
+router.post("/notes", (req, res) => {
     const note = req.body;
-    fs.readFile("../db/db.json", "utf8", function(err, data){
-    //console.log(data)
-        const notes = [].concat(JSON.parse(data));
-        notes.push(note);
-        return notes
-    })
-    .then(function(notes){
-        fs.writeFile("../db/db.json", JSON.stringify(notes))
-        res.json(note);
-    })
-})
+    notes.push(note);
+    
+    fs.writeFile("../db/db.json", JSON.stringify(notes))
+    
+    //     res.json(note);
+    // // })
+    // })
+});
+    
 
 module.exports = router
-
